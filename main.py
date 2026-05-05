cf42c6dad92c2487796e0a1f78341a51e5243b57
ArtemE54 <erosuinartem@gmail.com>
2026-05-05 18:33:58 +0700

    FastAPI_KonvaJS

---
+++ main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import JSONResponse
+app = FastAPI()
+scene_storage = None
+@app.post("/api/save")
+async def save_scene(request: Request):
+    global scene_storage
+    scene_storage = await request.json()
+    return {"status": "ok"}
+@app.get("/api/load")
+async def load_scene():
+    if scene_storage is None:
+        return JSONResponse(content={"error": "Нет сцены"})
+    return scene_storage
+app.mount("/", StaticFiles(directory="kon", html=True), name="kon")
+
+#uvicorn main:app --reload
